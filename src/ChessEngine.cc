8078b7a40242c4a6d3864cbb598a421c9b2afc1c
Entropy675 <sibteali@outlook.com>
2023-08-23 15:18:22 -0400

    Pushing forward the command queue a bit...

--- src/ChessEngine.cc
+++ src/ChessEngine.cc
@@ -44,32 +44,54 @@ void ChessEngine::responseLoop()
 {
 	int pingCounter = 0;
 	std::string data;
+	std::string command;
+	char timeBuffer[100];
+	std::time_t currentTime;
 	
 	while(true)
 	{
+		command = "\0";
+		
 		{	
 			std::lock_guard<std::mutex> lock(SocketCtrl->queueMutex);
 			std::queue<std::string>& cmdQueue = SocketCtrl->accessCommandQueue(lock);
 			// CORE LOOP
 			if(!cmdQueue.empty())
 			{
-				std::cout << cmdQueue.front() << std::endl;
+				command = cmdQueue.front();
 				cmdQueue.pop();
 			}
-			else if(pingCounter++ > 200)
-			{
-				pingCounter = 0;
-				std::cout << "Ping... " << std::endl;
-			}
 		}
+
+		// process command here
+		if(!command.empty())
+		{
+			std::cout << command << std::endl;
+		}
+		// Get the current time
+		currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		std::strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d %H:%M:%S", std::localtime(&currentTime));
+		
+		if(pingCounter++ > 200)
+		{
+			pingCounter = 0;
+			std::cout << "Ping... " << timeBuffer << std::endl;
+			// ping on the server side after 200 iterations...
+		}
+		
+
 		data = "ping... " + std::to_string(pingCounter) + " -> ";
 		
 		for(int i = 0; i < pingCounter; i++)
 			if(i % 4 == 0)
 				data += std::to_string(i);
 		
-		SocketCtrl->sendData(data);
+		// ping on the clients that are connected
+		SocketCtrl->sendData(data); // respond all clients connected
+		// can also do SocketCtrl->sendData(data, socket); for specific comms.
 		
+		
+		// wait for delay...
 		std::this_thread::sleep_for(std::chrono::milliseconds(ENGINE_DELAY_MS));
 	}
 }
